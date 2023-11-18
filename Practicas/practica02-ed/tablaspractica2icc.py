bbb9f8120205c73b8731949c669087d3437122db
Epharedam <129038355+Epharedam@users.noreply.github.com>
2023-11-18 03:21:28 -0600

    practicas

---
+++ Practicas/practica02-ed/tablaspractica2icc.py
@@ -0,0 +1,4 @@
+for numero1 in range(1, 11):
+  for numero2 in range(1, 11):
+    print(numero1, "*", numero2, "=", numero1 * numero2)
+  
---
+++ Practicas/practica04-ed/piedrapapeltijeras.py
@@ -0,0 +1,11 @@
+#usaré un código basado en el que hicimos la práctica 2
+
+def PPoT(piedrapapeltijeras):
+    if piedrapapeltijeras == "piedra":
+        return "papel"
+    elif piedrapapeltijeras == "papel":
+        return "tijeras"
+    elif piedrapapeltijeras == "tijeras":
+        return "piedra"
+    else:
+        return "no es un movimiento valido °՞(ᗒᗣᗕ)՞°"
